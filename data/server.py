706189aae44836cfbad9d38b2c3dc87b16801bbb
stephaniewang26 <stephanie.wang26@trinityschoolnyc.org>
2024-04-19 10:40:57 -0400

    update 4/19

--- data/server.py
+++ data/server.py
@@ -10,20 +10,29 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def index():
-    return render_template('about.html')
+    f = open("data/tripsperday_taxi_rideshare/general_rideshare_data.json")
+    gen_rideshare_data = json.load(f)
+    f.close()
+    requested_year = request.args.get('year')
+    years = sorted(list(gen_rideshare_data.keys()))
+    return render_template('about.html',year=requested_year, all_years=years)
 
 @app.route('/macro')
 def macro():
-    return render_template('macro.html')
+    f = open("data/tripsperday_taxi_rideshare/general_rideshare_data.json")
+    gen_rideshare_data = json.load(f)
+    f.close()
+    requested_year = request.args.get('year')
+    years = sorted(list(gen_rideshare_data.keys()))
+    return render_template('macro.html',year=requested_year, all_years=years)
 
 @app.route('/micro')
 def micro():
     f = open("data/tripsperday_taxi_rideshare/general_rideshare_data.json")
     gen_rideshare_data = json.load(f)
     f.close()
-
-    #Check to see if year is passed via the query string portion of the URL
     requested_year = request.args.get('year')
-    return render_template('micro.html',year=requested_year)
+    years = sorted(list(gen_rideshare_data.keys()))
+    return render_template('micro.html',year=requested_year, all_years=years)
 
 app.run(debug=True)
