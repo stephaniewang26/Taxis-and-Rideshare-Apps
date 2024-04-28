a0e625ac2421fe39add27b09e86399b5f99b4803
stephaniewang26 <stephanie.wang26@trinityschoolnyc.org>
2024-04-28 00:04:34 -0400

    update 4/28

--- data/reformatDataMacro.py
+++ data/reformatDataMacro.py
@@ -2,7 +2,7 @@ import json
 
 #MICRO
 
-f = open("taxi_rideshare_data.csv", "r")
+f = open(r"D:\New folder\Taxis-and-Rideshare-Apps-1\data\tripsperday_taxi_rideshare\taxi_rideshare_data.csv", "r")
 lines = f.readlines()
 taxi_dictionary = {}
 general_rideshare_dictionary = {}
@@ -14,30 +14,40 @@ for line in lines:
 
     line[7] = line[7].replace("\n","")
     current_year = line[0][:4]
-    current_month = line[0][5:7]
+    if (line[0][5:7])[0] != "0":
+        current_month = line[0][5:7]
+    else:
+        current_month = line[0][6:7]
+    if current_month != "/Y":
+        current_month = int(current_month)
+    print(type(current_month))
 
     if line[1] == "Yellow":
         if current_year not in taxi_dictionary:
             taxi_dictionary[current_year] = dict()
+            taxi_dictionary[current_year][current_month] = dict()
+        elif current_month not in taxi_dictionary[current_year]:
+            taxi_dictionary[current_year][current_month] = dict()
         #print(line[2])
-        taxi_dictionary[current_year][current_month] = [("trips per day:",line[2]),
-                                                        ("farebox per day:",line[3]),
-                                                        ("unique vehicles:",line[4]),
-                                                        ("vehicles per day:",line[5]),
-                                                        ("avg hrs/day per vehicle:",line[6]),
-                                                        ("avg mins/trip:",line[7]),
-                                                        ] 
+        taxi_dictionary[current_year][current_month]["trips per day"] = line[2]
+        taxi_dictionary[current_year][current_month]["farebox per day"] = line[3]
+        taxi_dictionary[current_year][current_month]["unique vehicles"] = line[4]
+        taxi_dictionary[current_year][current_month]["vehicles per day"] = line[5]
+        taxi_dictionary[current_year][current_month]["avg hrs per day per vehicle"] = line[6]
+        taxi_dictionary[current_year][current_month]["avg mins per trip"] = line[7]
 
     if line[1] == "FHV - High Volume":
         if current_year not in general_rideshare_dictionary:
             general_rideshare_dictionary[current_year] = dict()
-        general_rideshare_dictionary[current_year][current_month] = [("trips per day:",line[2]),
-                                                        ("farebox per day:",line[3]),
-                                                        ("unique vehicles:",line[4]),
-                                                        ("vehicles per day:",line[5]),
-                                                        ("avg hrs/day per vehicle:",line[6]),
-                                                        ("avg mins/trip:",line[7]),
-                                                        ] 
+            general_rideshare_dictionary[current_year][current_month] = dict()
+        elif current_month not in general_rideshare_dictionary[current_year]:
+            general_rideshare_dictionary[current_year][current_month] = dict()
+        general_rideshare_dictionary[current_year][current_month]["trips per day"] = line[2]
+        general_rideshare_dictionary[current_year][current_month]["farebox per day"] = line[3]
+        general_rideshare_dictionary[current_year][current_month]["unique vehicles"] = line[4]
+        general_rideshare_dictionary[current_year][current_month]["vehicles per day"] = line[5]
+        general_rideshare_dictionary[current_year][current_month]["avg hrs per day per vehicle"] = line[6]
+        general_rideshare_dictionary[current_year][current_month]["avg mins per trip"] = line[7]
 
 #Save the json object to a file
 f2 = open("taxi_data.json", "w")
--- data/server.py
+++ data/server.py
@@ -27,18 +27,35 @@ def macro():
     taxi_data = json.load(f)
     f.close()
 
+    gen_rideshare_data = dict(reversed(gen_rideshare_data.items()))
+    taxi_data = dict(reversed(taxi_data.items()))
+
     requested_year = request.args.get('year')
     all_years = sorted(list(gen_rideshare_data.keys()))
     tripsbyk = ["800,000","700,000","600,000","500,000","400,000","300,000","200,000","100,000",]
 
-    taxi_trip_values = {}
-    for key in taxi_data:
-        pass
-     #reformat data!!!
-    # #Filter and reformat data for ease of access in the template
-    # requested_data = data[requested_pet]
-    # years = sorted(list(requested_data.keys()))
     taxi_line_endpoints =[]
+    taxi_trip_values = {}
+    connect_x = None
+    for year in taxi_data:
+        taxi_data[year] = dict(reversed(taxi_data[year].items()))
+        for month in taxi_data[year]:
+            if month == "1" and connect_x != None:
+                stop_x = taxi_data[year][month]["trips per day"]
+                taxi_line_endpoints.append([connect_x,stop_x])
+
+                start_x = taxi_data[year][month]["trips per day"]
+                stop_x = taxi_data[year][str(int(month)+1)]["trips per day"]
+                taxi_line_endpoints.append([start_x,stop_x])
+            elif month == "12":
+                connect_x = taxi_data[year][month]["trips per day"]
+            else:
+                start_x = taxi_data[year][month]["trips per day"]
+                stop_x = taxi_data[year][str(int(month)+1)]["trips per day"]
+                taxi_line_endpoints.append([start_x,stop_x])
+    
+    print(taxi_line_endpoints)
+    
 
     # for i in range(len(all_years)-1): # make it easy to dynamically generate a line graph
     #     start_y = all_years[i] #generate endpoints for each line segment
