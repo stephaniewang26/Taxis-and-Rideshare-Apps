412c5b8d1aa201f0390844ae6b469c27b43b79b7
stephaniewang26 <stephanie.wang26@trinityschoolnyc.org>
2024-05-01 10:49:01 -0400

    update 5/1

--- data/combine_yearly_data.py
+++ data/combine_yearly_data.py
@@ -3,10 +3,13 @@ import json
 #https://medium.com/@abdelfatahmennoun4/how-to-combine-multiple-json-files-into-a-single-json-file-c2ed3dc372c2
 # Create a list of all the JSON files that you want to combine.
 json_files = []
+starting = "data/yearly_yellowtaxi_full_json/"
 ending = "_yellowtaxi.json"
 
 for i in range (2016,2023):
-    json_files.append(str(i)+ending)
+    json_files.append(starting+str(i)+ending)
+
+print(json_files)
 
 # Create an empty list to store the Python objects.
 python_objects = []
